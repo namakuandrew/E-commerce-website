5e5f7ab426ff8a154e56488dd9b08b63d81b7023
namakuandrew <thesky11.lay@gmail.com>
2023-07-08 15:53:31 +0700

    extra css and java

---
+++ script.js
@@ -0,0 +1,5 @@
+let menubtn = document.getElementById("menuBtn");
+let sideNav = document.getElementById("sideNav");
+let menu = document.getElementById("menu");
+
+//disini kita atur logika tombolnya
