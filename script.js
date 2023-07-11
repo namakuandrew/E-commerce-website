be3b3cb1ec1c86ba1f55be262f11105e39288ec0
namakuandrew <thesky11.lay@gmail.com>
2023-07-11 14:56:27 +0700

    extra html and javascript

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ let sideNav = document.getElementById("sideNav");
 let menu = document.getElementById("menu");
 
 //disini kita atur logika tombolnya
+menubtn.onclick = function () {
+  if (sideNav.style.right == "-250px") {
+    sideNav.style.right = "0";
+    menu.src = "images/close.png";
+  } else {
+    sideNav.style.right = "-250px";
+    menu.src = "images/menu.png";
+  }
+};
+
+let i = 0;
+let t = "give yourself \n a new style this year";
+
+function typing() {
+  if (i < t.length) {
+    document.getElementById("text").innerHTML += t.charAt(i);
+    i++;
+    setTimeout(typing, 100);
+  }
+}
+
+typing();
