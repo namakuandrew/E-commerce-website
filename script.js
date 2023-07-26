a21498b5b5470ffa553d0f4b20d59cdd8d61b71b
namakuandrew <thesky11.lay@gmail.com>
2023-07-26 12:47:56 +0700

    make website responsive

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ let sideNav = document.getElementById("sideNav");
 let menu = document.getElementById("menu");
 
 //disini kita atur logika tombolnya
-menubtn.onclick = function () {
-  if (sideNav.style.right == "-250px") {
+sideNav.style.right = "-250px";
+function togglemenu() {
+  if (sideNav.style.right === "-250px") {
     sideNav.style.right = "0";
     menu.src = "images/close.png";
   } else {
     sideNav.style.right = "-250px";
     menu.src = "images/menu.png";
   }
-};
+}
 
+menubtn.addEventListener("click", togglemenu);
+
+function openimg(element) {
+  window.open(element.src);
+}
+
+// for typing effect animation
 let i = 0;
 let t = "give yourself \n a new style this year";
 
